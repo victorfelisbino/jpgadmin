7517018418e8319b94eec83066e8f5e99f788d62
Victor Felisbino dias <victor.felisbino@lvc02td77xgtfl.local>
2017-05-25 15:23:49 -0500

    addind update

--- src/com/products/UploadCSV.java
+++ src/com/products/UploadCSV.java
@@ -74,57 +74,63 @@ public class UploadCSV extends HttpServlet {
 		     PreparedStatement ps=con.prepareStatement(sql);
 		     while ((nextLine = csvReader.readNext()) != null) {
 		    	 // need to check sku first and decide to update row or insert new
+		    	 //if dist_sku exist in the product table for vendor, update
 		    	 
-	    		 ps.setString(1, csvFieldsMap.get(mappingMap.get("VENDOR_SKU")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("VENDOR_SKU"))]: ""); //VENDOR SKU 
-	    		 ps.setString(2, csvFieldsMap.get(mappingMap.get("DIST_SKU")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("DIST_SKU"))]: ""); //DIST SKU
-	    		 ps.setString(3, "PETRA"); //VENDOR
-	    		 ps.setString(4, csvFieldsMap.get(mappingMap.get("SHORT_DESC")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("SHORT_DESC"))]: ""); //SHORT DESC
-				 ps.setString(5, csvFieldsMap.get(mappingMap.get("AVAILABLE")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("AVAILABLE"))] : ""); //AVAILABLE
-				 ps.setDouble(6, Double.valueOf(csvFieldsMap.get(mappingMap.get("DIST_COST")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("DIST_COST"))] : "")); //DIST COST
-				 ps.setString(7, csvFieldsMap.get(mappingMap.get("BRAND_NAME")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("BRAND_NAME"))] : ""); //BRAND NAME, 
-				 ps.setString(8, csvFieldsMap.get(mappingMap.get("WEIGHT_UNPACKED")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("WEIGHT_UNPACKED"))] : ""); //WEIGHT-UNPACKED, 
-				 ps.setString(9, csvFieldsMap.get(mappingMap.get("PRODUCT_CLASS")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("PRODUCT_CLASS"))] : ""); //PRODUCT CLASS, 
-				 ps.setString(10, csvFieldsMap.get(mappingMap.get("UPC")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("UPC"))] : ""); //UPC, 
-				 ps.setString(11, csvFieldsMap.get(mappingMap.get("LONG_DESC")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("LONG_DESC"))] : ""); //LONG DESC, 
-				 ps.setString(12, csvFieldsMap.get(mappingMap.get("KEYWORDS")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("KEYWORDS"))] : ""); //KEYWORDS, 
-				 ps.setString(13, csvFieldsMap.get(mappingMap.get("SPECS")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("SPECS"))] : ""); //SPECS, 
-				 ps.setString(14, csvFieldsMap.get(mappingMap.get("SUBCATEGORY")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("SUBCATEGORY"))] : ""); //SUBCATEGORY, 
-				 ps.setString(15, csvFieldsMap.get(mappingMap.get("NOTES")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("NOTES"))] : ""); //NOTES, 
-				 ps.setString(16, csvFieldsMap.get(mappingMap.get("REFURB")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("REFURB"))] : ""); //REFURB, 
-				 ps.setDouble(17, Double.valueOf(csvFieldsMap.get(mappingMap.get("LENGTH")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("LENGTH"))] : "")); //LENGTH, 
-				 ps.setDouble(18, Double.valueOf(csvFieldsMap.get(mappingMap.get("WIDTH")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("WIDTH"))] : "")); //WIDTH, 
-				 ps.setDouble(19, Double.valueOf(csvFieldsMap.get(mappingMap.get("HEIGHT")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("HEIGHT"))] : "")); //HEIGHT, 
-				 ps.setDouble(20, Double.valueOf(csvFieldsMap.get(mappingMap.get("MAP")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("MAP"))] : "")); //MAP, 
-				 ps.setString(21, csvFieldsMap.get(mappingMap.get("RETURNABLE")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("RETURNABLE"))] : ""); //RETURNABLE, 
-				 ps.setDouble(22, Double.valueOf(csvFieldsMap.get(mappingMap.get("ESTIMATED_SHIP_WEIGHT")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("ESTIMATED_SHIP_WEIGHT"))] : "")); //ESTIMATED SHIP WEIGHT, 
-				 ps.setString(23, csvFieldsMap.get(mappingMap.get("NOTES2")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("NOTES2"))] : ""); //NOTES2, 
-				 ps.setString(24, csvFieldsMap.get(mappingMap.get("SUBCATEGORY2")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("SUBCATEGORY2"))] : ""); //SUBCATEGORY2, 
-				 ps.setString(25, csvFieldsMap.get(mappingMap.get("SUBCATEGORY3")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("SUBCATEGORY3"))] : ""); //SUBCATEGORY3, 
-				 ps.setDouble(26, Double.valueOf(csvFieldsMap.get(mappingMap.get("MSRP")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("MSRP"))] : "")); //MSRP, 
-				 if(csvFieldsMap.get(mappingMap.get("PO_ETA_DATE")) != null && !nextLine[csvFieldsMap.get(mappingMap.get("PO_ETA_DATE"))].isEmpty()){
-					 try{
-						 ps.setDate(27, java.sql.Date.valueOf(nextLine[csvFieldsMap.get(mappingMap.get("PO_ETA_DATE"))]));//PO ETA DATE,
-					 }catch(Exception poeta){
+		    	 Boolean update = checkProduct("PETRA", csvFieldsMap.get(mappingMap.get("DIST_SKU")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("DIST_SKU"))]: "");
+	    		 if(update){
+	    			 
+	    		 }else{
+			    	 ps.setString(1, csvFieldsMap.get(mappingMap.get("VENDOR_SKU")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("VENDOR_SKU"))]: ""); //VENDOR SKU 
+		    		 ps.setString(2, csvFieldsMap.get(mappingMap.get("DIST_SKU")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("DIST_SKU"))]: ""); //DIST SKU
+		    		 ps.setString(3, "PETRA"); //VENDOR
+		    		 ps.setString(4, csvFieldsMap.get(mappingMap.get("SHORT_DESC")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("SHORT_DESC"))]: ""); //SHORT DESC
+					 ps.setString(5, csvFieldsMap.get(mappingMap.get("AVAILABLE")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("AVAILABLE"))] : ""); //AVAILABLE
+					 ps.setDouble(6, Double.valueOf(csvFieldsMap.get(mappingMap.get("DIST_COST")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("DIST_COST"))] : "")); //DIST COST
+					 ps.setString(7, csvFieldsMap.get(mappingMap.get("BRAND_NAME")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("BRAND_NAME"))] : ""); //BRAND NAME, 
+					 ps.setString(8, csvFieldsMap.get(mappingMap.get("WEIGHT_UNPACKED")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("WEIGHT_UNPACKED"))] : ""); //WEIGHT-UNPACKED, 
+					 ps.setString(9, csvFieldsMap.get(mappingMap.get("PRODUCT_CLASS")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("PRODUCT_CLASS"))] : ""); //PRODUCT CLASS, 
+					 ps.setString(10, csvFieldsMap.get(mappingMap.get("UPC")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("UPC"))] : ""); //UPC, 
+					 ps.setString(11, csvFieldsMap.get(mappingMap.get("LONG_DESC")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("LONG_DESC"))] : ""); //LONG DESC, 
+					 ps.setString(12, csvFieldsMap.get(mappingMap.get("KEYWORDS")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("KEYWORDS"))] : ""); //KEYWORDS, 
+					 ps.setString(13, csvFieldsMap.get(mappingMap.get("SPECS")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("SPECS"))] : ""); //SPECS, 
+					 ps.setString(14, csvFieldsMap.get(mappingMap.get("SUBCATEGORY")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("SUBCATEGORY"))] : ""); //SUBCATEGORY, 
+					 ps.setString(15, csvFieldsMap.get(mappingMap.get("NOTES")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("NOTES"))] : ""); //NOTES, 
+					 ps.setString(16, csvFieldsMap.get(mappingMap.get("REFURB")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("REFURB"))] : ""); //REFURB, 
+					 ps.setDouble(17, Double.valueOf(csvFieldsMap.get(mappingMap.get("LENGTH")) != null && !nextLine[csvFieldsMap.get(mappingMap.get("LENGTH"))].isEmpty() ? nextLine[csvFieldsMap.get(mappingMap.get("LENGTH"))] : "0")); //LENGTH, 
+					 ps.setDouble(18, Double.valueOf(csvFieldsMap.get(mappingMap.get("WIDTH")) != null && !nextLine[csvFieldsMap.get(mappingMap.get("WIDTH"))].isEmpty() ? nextLine[csvFieldsMap.get(mappingMap.get("WIDTH"))] : "0")); //WIDTH, 
+					 ps.setDouble(19, Double.valueOf(csvFieldsMap.get(mappingMap.get("HEIGHT")) != null && !nextLine[csvFieldsMap.get(mappingMap.get("HEIGHT"))].isEmpty() ? nextLine[csvFieldsMap.get(mappingMap.get("HEIGHT"))] : "0")); //HEIGHT, 
+					 ps.setDouble(20, Double.valueOf(csvFieldsMap.get(mappingMap.get("MAP")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("MAP"))] : "0")); //MAP, 
+					 ps.setString(21, csvFieldsMap.get(mappingMap.get("RETURNABLE")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("RETURNABLE"))] : ""); //RETURNABLE, 
+					 ps.setDouble(22, Double.valueOf(csvFieldsMap.get(mappingMap.get("ESTIMATED_SHIP_WEIGHT")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("ESTIMATED_SHIP_WEIGHT"))] : "0")); //ESTIMATED SHIP WEIGHT, 
+					 ps.setString(23, csvFieldsMap.get(mappingMap.get("NOTES2")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("NOTES2"))] : ""); //NOTES2, 
+					 ps.setString(24, csvFieldsMap.get(mappingMap.get("SUBCATEGORY2")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("SUBCATEGORY2"))] : ""); //SUBCATEGORY2, 
+					 ps.setString(25, csvFieldsMap.get(mappingMap.get("SUBCATEGORY3")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("SUBCATEGORY3"))] : ""); //SUBCATEGORY3, 
+					 ps.setDouble(26, Double.valueOf(csvFieldsMap.get(mappingMap.get("MSRP")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("MSRP"))] : "0")); //MSRP, 
+					 if(csvFieldsMap.get(mappingMap.get("PO_ETA_DATE")) != null && !nextLine[csvFieldsMap.get(mappingMap.get("PO_ETA_DATE"))].isEmpty()){
+						 try{
+							 ps.setDate(27, java.sql.Date.valueOf(nextLine[csvFieldsMap.get(mappingMap.get("PO_ETA_DATE"))]));//PO ETA DATE,
+						 }catch(Exception poeta){
+							 ps.setDate(27, null);//PO ETA DATE,
+						 }
+					 }else{
 						 ps.setDate(27, null);//PO ETA DATE,
 					 }
-				 }else{
-					 ps.setDate(27, null);//PO ETA DATE,
-				 }
-				 ps.setString(28, csvFieldsMap.get(mappingMap.get("WARRANTY")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("WARRANTY"))] : ""); //WARRANTY, 
-				 ps.setString(29, csvFieldsMap.get(mappingMap.get("IMAGE_URL")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("IMAGE_URL"))] : ""); //IMAGE URL,
-				 ps.setFloat(30, 0); //AMAZON COST TO SELL, 
-				 ps.setFloat(31, 0); //PERCENT MARKUP, 
-				 ps.setFloat(32, 0); //PROFIT, 
-				 ps.setFloat(33, 0); //AMAZON SELLING PRICE, 
-				 ps.setString(34, csvFieldsMap.get(mappingMap.get("SELLING_ON_AMAZON")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("SELLING_ON_AMAZON"))] : ""); //SELLING ON AMAZON, 
-				 ps.setString(35, csvFieldsMap.get(mappingMap.get("DROP_SHIP")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("DROP_SHIP"))] : ""); //DROP SHIP, 
-				 ps.setString(36, csvFieldsMap.get(mappingMap.get("SELL_PRODUCT")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("SELL_PRODUCT"))] : ""); //SELL PRODUCT, 
-				 ps.setString(37, csvFieldsMap.get(mappingMap.get("PRODUCT_DISCONTINUED")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("PRODUCT_DISCONTINUED"))] : ""); //PRODUCT DISCONTINUED, 
-				 ps.setString(38, csvFieldsMap.get(mappingMap.get("MODIFIED")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("MODIFIED"))] : ""); //MODIFIED, 
-				 ps.setDate(39, java.sql.Date.valueOf(java.time.LocalDate.now())); //DATE MODIFIED, 
-				 ps.setString(40, csvFieldsMap.get(mappingMap.get("REVIEW_NEEDED")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("REVIEW_NEEDED"))] : ""); //REVIEW NEEDED, 
-				 ps.setString(41, csvFieldsMap.get(mappingMap.get("MODIFIED_FIELDS")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("MODIFIED_FIELDS"))] : ""); //MODIFIED FIELDS
-				 ps.executeUpdate();
+					 ps.setString(28, csvFieldsMap.get(mappingMap.get("WARRANTY")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("WARRANTY"))] : ""); //WARRANTY, 
+					 ps.setString(29, csvFieldsMap.get(mappingMap.get("IMAGE_URL")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("IMAGE_URL"))] : ""); //IMAGE URL,
+					 ps.setFloat(30, 0); //AMAZON COST TO SELL, 
+					 ps.setFloat(31, 0); //PERCENT MARKUP, 
+					 ps.setFloat(32, 0); //PROFIT, 
+					 ps.setFloat(33, 0); //AMAZON SELLING PRICE, 
+					 ps.setString(34, csvFieldsMap.get(mappingMap.get("SELLING_ON_AMAZON")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("SELLING_ON_AMAZON"))] : ""); //SELLING ON AMAZON, 
+					 ps.setString(35, csvFieldsMap.get(mappingMap.get("DROP_SHIP")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("DROP_SHIP"))] : ""); //DROP SHIP, 
+					 ps.setString(36, csvFieldsMap.get(mappingMap.get("SELL_PRODUCT")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("SELL_PRODUCT"))] : ""); //SELL PRODUCT, 
+					 ps.setString(37, csvFieldsMap.get(mappingMap.get("PRODUCT_DISCONTINUED")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("PRODUCT_DISCONTINUED"))] : ""); //PRODUCT DISCONTINUED, 
+					 ps.setString(38, csvFieldsMap.get(mappingMap.get("MODIFIED")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("MODIFIED"))] : ""); //MODIFIED, 
+					 ps.setDate(39, java.sql.Date.valueOf(java.time.LocalDate.now())); //DATE MODIFIED, 
+					 ps.setString(40, csvFieldsMap.get(mappingMap.get("REVIEW_NEEDED")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("REVIEW_NEEDED"))] : ""); //REVIEW NEEDED, 
+					 ps.setString(41, csvFieldsMap.get(mappingMap.get("MODIFIED_FIELDS")) != null ? nextLine[csvFieldsMap.get(mappingMap.get("MODIFIED_FIELDS"))] : ""); //MODIFIED FIELDS
+					 ps.executeUpdate();
+	    		 }
 		     }
 		     response.sendRedirect("fileUpload.jsp"); 
 		}catch (Exception theException){
@@ -199,5 +205,28 @@ public class UploadCSV extends HttpServlet {
 			e.printStackTrace();
 		}
 		return mappingMap;
+	}
+	public static HashMap<String,String> existingProdMap;
+	public Boolean checkProduct(String vendor, String dist_sku){
+		Boolean exists = false;
+		if(existingProdMap == null){
+			try{
+				existingProdMap = new HashMap<String,String>();
+				Connection con=ConnectionManager.getConnection();
+				PreparedStatement ps = con.prepareStatement("select dist_sku,id from products where vendor = \'" + vendor + "\'");
+				ResultSet rs=ps.executeQuery();
+				while(rs.next()){
+					existingProdMap.put(rs.getString("dist_sku"), rs.getString("id"));
+				}
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+		}else{
+			if(existingProdMap.get(dist_sku) != null){
+				exists = true;
+			}
+		}
+		
+		return exists;
 	}
 }
