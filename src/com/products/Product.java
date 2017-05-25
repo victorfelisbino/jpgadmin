6a74388226065ab627ede46423bddec6b9664cc4
victor.felisbino <victor.felisbino@172.17.69.150>
2017-05-25 16:56:44 -0500

    changes

---
+++ src/com/products/Product.java
@@ -0,0 +1,293 @@
+package com.products;
+
+ public class Product {
+	
+	 private String VENDOR_SKU; 
+	 private String DIST_SKU;
+	 private String VENDOR;
+	 private String SHORT_DESC;
+	 private String AVAILABLE;
+	 private String DIST_COST;
+	 private String BRAND_NAME;
+	 private String WEIGHT_UNPACKED;
+	 private String PRODUCT_CLASS;
+	 private String UPC;
+	 private String LONG_DESC;
+	 private String KEYWORDS;
+	 private String SPECS;
+	 private String SUBCATEGORY;
+	 private String NOTES;
+	 private String REFURB;
+	 private String LENGTH;
+	 private String WIDTH;
+	 private String HEIGHT;
+	 private String MAP;
+	 private String RETURNABLE;
+	 private String ESTIMATED_SHIP_WEIGHT;
+	 private String NOTES2;
+	 private String SUBCATEGORY2;
+	 private String SUBCATEGORY3;
+	 private String MSRP;
+	 private String PO_ETA_DATE;
+	 private String WARRANTY;
+	 private String IMAGE_URL;
+	 private String AMAZON_COST_TO_SELL;
+	 private String PERCENT_MARKUP;
+	 private String PROFIT;
+	 private String AMAZON_SELLING_PRICE;
+	 private String SELLING_ON_AMAZON;
+	 private String DROP_SHIP;
+	 private String SELL_PRODUCT;
+	 private String PRODUCT_DISCONTINUED;
+	 private String MODIFIED;
+	 private String DATE_MODIFIED;
+	 private String REVIEW_NEEDED;
+	 private String MODIFIED_FIELDS;
+	public String getVENDOR_SKU() {
+		return VENDOR_SKU;
+	}
+	public String getDIST_SKU() {
+		return DIST_SKU;
+	}
+	public String getVENDOR() {
+		return VENDOR;
+	}
+	public String getSHORT_DESC() {
+		return SHORT_DESC;
+	}
+	public String getAVAILABLE() {
+		return AVAILABLE;
+	}
+	public String getDIST_COST() {
+		return DIST_COST;
+	}
+	public String getBRAND_NAME() {
+		return BRAND_NAME;
+	}
+	public String getWEIGHT_UNPACKED() {
+		return WEIGHT_UNPACKED;
+	}
+	public String getPRODUCT_CLASS() {
+		return PRODUCT_CLASS;
+	}
+	public String getUPC() {
+		return UPC;
+	}
+	public String getLONG_DESC() {
+		return LONG_DESC;
+	}
+	public String getKEYWORDS() {
+		return KEYWORDS;
+	}
+	public String getSPECS() {
+		return SPECS;
+	}
+	public String getSUBCATEGORY() {
+		return SUBCATEGORY;
+	}
+	public String getNOTES() {
+		return NOTES;
+	}
+	public String getREFURB() {
+		return REFURB;
+	}
+	public String getLENGTH() {
+		return LENGTH;
+	}
+	public String getWIDTH() {
+		return WIDTH;
+	}
+	public String getHEIGHT() {
+		return HEIGHT;
+	}
+	public String getMAP() {
+		return MAP;
+	}
+	public String getRETURNABLE() {
+		return RETURNABLE;
+	}
+	public String getESTIMATED_SHIP_WEIGHT() {
+		return ESTIMATED_SHIP_WEIGHT;
+	}
+	public String getNOTES2() {
+		return NOTES2;
+	}
+	public String getSUBCATEGORY2() {
+		return SUBCATEGORY2;
+	}
+	public String getSUBCATEGORY3() {
+		return SUBCATEGORY3;
+	}
+	public String getMSRP() {
+		return MSRP;
+	}
+	public String getPO_ETA_DATE() {
+		return PO_ETA_DATE;
+	}
+	public String getWARRANTY() {
+		return WARRANTY;
+	}
+	public String getIMAGE_URL() {
+		return IMAGE_URL;
+	}
+	public String getAMAZON_COST_TO_SELL() {
+		return AMAZON_COST_TO_SELL;
+	}
+	public String getPERCENT_MARKUP() {
+		return PERCENT_MARKUP;
+	}
+	public String getPROFIT() {
+		return PROFIT;
+	}
+	public String getAMAZON_SELLING_PRICE() {
+		return AMAZON_SELLING_PRICE;
+	}
+	public String getSELLING_ON_AMAZON() {
+		return SELLING_ON_AMAZON;
+	}
+	public String getDROP_SHIP() {
+		return DROP_SHIP;
+	}
+	public String getSELL_PRODUCT() {
+		return SELL_PRODUCT;
+	}
+	public String getPRODUCT_DISCONTINUED() {
+		return PRODUCT_DISCONTINUED;
+	}
+	public String getMODIFIED() {
+		return MODIFIED;
+	}
+	public String getDATE_MODIFIED() {
+		return DATE_MODIFIED;
+	}
+	public String getREVIEW_NEEDED() {
+		return REVIEW_NEEDED;
+	}
+	public String getMODIFIED_FIELDS() {
+		return MODIFIED_FIELDS;
+	}
+	public void setVENDOR_SKU(String vENDOR_SKU) {
+		VENDOR_SKU = vENDOR_SKU;
+	}
+	public void setDIST_SKU(String dIST_SKU) {
+		DIST_SKU = dIST_SKU;
+	}
+	public void setVENDOR(String vENDOR) {
+		VENDOR = vENDOR;
+	}
+	public void setSHORT_DESC(String sHORT_DESC) {
+		SHORT_DESC = sHORT_DESC;
+	}
+	public void setAVAILABLE(String aVAILABLE) {
+		AVAILABLE = aVAILABLE;
+	}
+	public void setDIST_COST(String dIST_COST) {
+		DIST_COST = dIST_COST;
+	}
+	public void setBRAND_NAME(String bRAND_NAME) {
+		BRAND_NAME = bRAND_NAME;
+	}
+	public void setWEIGHT_UNPACKED(String wEIGHT_UNPACKED) {
+		WEIGHT_UNPACKED = wEIGHT_UNPACKED;
+	}
+	public void setPRODUCT_CLASS(String pRODUCT_CLASS) {
+		PRODUCT_CLASS = pRODUCT_CLASS;
+	}
+	public void setUPC(String uPC) {
+		UPC = uPC;
+	}
+	public void setLONG_DESC(String lONG_DESC) {
+		LONG_DESC = lONG_DESC;
+	}
+	public void setKEYWORDS(String kEYWORDS) {
+		KEYWORDS = kEYWORDS;
+	}
+	public void setSPECS(String sPECS) {
+		SPECS = sPECS;
+	}
+	public void setSUBCATEGORY(String sUBCATEGORY) {
+		SUBCATEGORY = sUBCATEGORY;
+	}
+	public void setNOTES(String nOTES) {
+		NOTES = nOTES;
+	}
+	public void setREFURB(String rEFURB) {
+		REFURB = rEFURB;
+	}
+	public void setLENGTH(String lENGTH) {
+		LENGTH = lENGTH;
+	}
+	public void setWIDTH(String wIDTH) {
+		WIDTH = wIDTH;
+	}
+	public void setHEIGHT(String hEIGHT) {
+		HEIGHT = hEIGHT;
+	}
+	public void setMAP(String mAP) {
+		MAP = mAP;
+	}
+	public void setRETURNABLE(String rETURNABLE) {
+		RETURNABLE = rETURNABLE;
+	}
+	public void setESTIMATED_SHIP_WEIGHT(String eSTIMATED_SHIP_WEIGHT) {
+		ESTIMATED_SHIP_WEIGHT = eSTIMATED_SHIP_WEIGHT;
+	}
+	public void setNOTES2(String nOTES2) {
+		NOTES2 = nOTES2;
+	}
+	public void setSUBCATEGORY2(String sUBCATEGORY2) {
+		SUBCATEGORY2 = sUBCATEGORY2;
+	}
+	public void setSUBCATEGORY3(String sUBCATEGORY3) {
+		SUBCATEGORY3 = sUBCATEGORY3;
+	}
+	public void setMSRP(String mSRP) {
+		MSRP = mSRP;
+	}
+	public void setPO_ETA_DATE(String pO_ETA_DATE) {
+		PO_ETA_DATE = pO_ETA_DATE;
+	}
+	public void setWARRANTY(String wARRANTY) {
+		WARRANTY = wARRANTY;
+	}
+	public void setIMAGE_URL(String iMAGE_URL) {
+		IMAGE_URL = iMAGE_URL;
+	}
+	public void setAMAZON_COST_TO_SELL(String aMAZON_COST_TO_SELL) {
+		AMAZON_COST_TO_SELL = aMAZON_COST_TO_SELL;
+	}
+	public void setPERCENT_MARKUP(String pERCENT_MARKUP) {
+		PERCENT_MARKUP = pERCENT_MARKUP;
+	}
+	public void setPROFIT(String pROFIT) {
+		PROFIT = pROFIT;
+	}
+	public void setAMAZON_SELLING_PRICE(String aMAZON_SELLING_PRICE) {
+		AMAZON_SELLING_PRICE = aMAZON_SELLING_PRICE;
+	}
+	public void setSELLING_ON_AMAZON(String sELLING_ON_AMAZON) {
+		SELLING_ON_AMAZON = sELLING_ON_AMAZON;
+	}
+	public void setDROP_SHIP(String dROP_SHIP) {
+		DROP_SHIP = dROP_SHIP;
+	}
+	public void setSELL_PRODUCT(String sELL_PRODUCT) {
+		SELL_PRODUCT = sELL_PRODUCT;
+	}
+	public void setPRODUCT_DISCONTINUED(String pRODUCT_DISCONTINUED) {
+		PRODUCT_DISCONTINUED = pRODUCT_DISCONTINUED;
+	}
+	public void setMODIFIED(String mODIFIED) {
+		MODIFIED = mODIFIED;
+	}
+	public void setDATE_MODIFIED(String dATE_MODIFIED) {
+		DATE_MODIFIED = dATE_MODIFIED;
+	}
+	public void setREVIEW_NEEDED(String rEVIEW_NEEDED) {
+		REVIEW_NEEDED = rEVIEW_NEEDED;
+	}
+	public void setMODIFIED_FIELDS(String mODIFIED_FIELDS) {
+		MODIFIED_FIELDS = mODIFIED_FIELDS;
+	}
+	
+}
--- src/com/products/ProductUtils.java
+++ src/com/products/ProductUtils.java
@@ -22,5 +22,53 @@ public class ProductUtils{
 		}
 		return vendorList;
 	}
+	int 	iTotalRecords;
 	
+	int 	iTotalDisplayRecords;
+	
+	String 	sEcho;
+	
+	String sColumns;
+	
+	List<Product> aaData;
+
+	public int getiTotalRecords() {
+		return iTotalRecords;
+	}
+
+	public void setiTotalRecords(int iTotalRecords) {
+		this.iTotalRecords = iTotalRecords;
+	}
+
+	public int getiTotalDisplayRecords() {
+		return iTotalDisplayRecords;
+	}
+
+	public void setiTotalDisplayRecords(int iTotalDisplayRecords) {
+		this.iTotalDisplayRecords = iTotalDisplayRecords;
+	}
+
+	public String getsEcho() {
+		return sEcho;
+	}
+
+	public void setsEcho(String sEcho) {
+		this.sEcho = sEcho;
+	}
+
+	public String getsColumns() {
+		return sColumns;
+	}
+
+	public void setsColumns(String sColumns) {
+		this.sColumns = sColumns;
+	}
+
+	public List<Product> getAaData() {
+		return aaData;
+	}
+
+	public void setAaData(List<Product> aaData) {
+		this.aaData = aaData;
+	}
 }
